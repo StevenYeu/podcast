d0e5921d3490367b48fd5d2467b276cbe30f6dfd
Steven Yeu <yeusteven@gmail.com>
2019-03-12 12:36:27 -0700

    added styling to title, add button to bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import PropTypes from 'prop-types';
+import './Navbar.scss';
 
-const Navbar = () => (
-  <nav className="navbar is-black" role="navigation" aria-label="main navigation">
-    <div className="navbar-brand">
-      <a className="navbar-item" href="https://versions.bulma.io/0.7.0">
-        <img
-          src="https://versions.bulma.io/0.7.0/images/bulma-logo-white.png"
-          alt="Bulma: a modern CSS framework based on Flexbox"
-          width="112"
-          height="28"
-        />
-      </a>
+const Navbar = ({ onClick }) => {
+  const [toggle, setToggle] = useState(false);
+  return (
+    <nav className="navbar is-black is-fixed-top" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item nav-title" href="https://versions.bulma.io/0.7.0">
+          Podcast Player
+        </a>
+        <a
+          role="button"
+          className={toggle ? 'navbar-burger burgers is-active' : 'navbar-burger burgers'}
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="menuButtons"
+          onClick={() => setToggle(!toggle)}
+        >
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+        </a>
+      </div>
 
-      <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
-        <span aria-hidden="true" />
-        <span aria-hidden="true" />
-        <span aria-hidden="true" />
-      </a>
-    </div>
-  </nav>
-);
+      <div className={toggle ? 'navbar-menu is-active' : 'navbar-menu'} id="menuButtons">
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <button type="button" className="button is-info" onClick={onClick}>
+              {' '}
+              <FontAwesomeIcon icon={faPlus} transform="left-4" />
+              Add Podcast
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  onClick: PropTypes.func,
+};
+
+Navbar.defaultProps = {
+  // eslint-disable-next-line no-console
+  onClick: () => console.log('Add'),
+};
 
 export default Navbar;
